d70ed4ed60f4f6a9adf2aa54a5cfdfd31f990bd9
notexplosive <thenotexplosive@gmail.com>
2022-06-08 11:55:42 -0700

    Basic tests

--- src/mygame/data/ingredient.ts
+++ src/mygame/data/ingredient.ts
@@ -5,7 +5,7 @@ export class Ingredient {
     readonly name: string;
     readonly index: number;
 
-    constructor(name: string, index: number) {
+    constructor(index: number, name: string) {
         this.name = name;
         this.index = index;
     }
@@ -15,20 +15,20 @@ export class Ingredient {
     }
 
     public static readonly All = [
-        new Ingredient("Fresh Autumn Leaf", 0),
-        new Ingredient("Wonderberry", 1),
-        new Ingredient("Hedgehog Fur", 2),
-        new Ingredient("Copy Bean", 3),
-        new Ingredient("Ice Dragon Tooth", 4),
-        new Ingredient("Whirlroot", 5),
-        new Ingredient("Chemical B", 6),
-        new Ingredient("Jellyfruit", 7),
-        new Ingredient("Lanky Mushroom", 8),
-        new Ingredient("Crumpleweed", 9),
-        new Ingredient("Wobby Booger", 10),
-        new Ingredient("Wigglekelp", 11),
-        new Ingredient("Motor Oil", 12),
-        new Ingredient("Happynut", 13),
-        new Ingredient("Funny Herb", 14),
+        new Ingredient(0, "Fresh Autumn Leaf"),
+        new Ingredient(1, "Wonderberry"),
+        new Ingredient(2, "Hedgehog Fur"),
+        new Ingredient(3, "Copy Bean"),
+        new Ingredient(4, "Ice Dragon Tooth"),
+        new Ingredient(5, "Whirlroot"),
+        new Ingredient(6, "Chemical B"),
+        new Ingredient(7, "Jellyfruit"),
+        new Ingredient(8, "Lanky Mushroom"),
+        new Ingredient(9, "Crumpleweed"),
+        new Ingredient(10, "Wobby Booger"),
+        new Ingredient(11, "Wigglekelp"),
+        new Ingredient(12, "Motor Oil"),
+        new Ingredient(13, "Happynut"),
+        new Ingredient(14, "Funny Herb"),
     ]
 }
---
+++ test/foo.test.ts
@@ -0,0 +1,5 @@
+import { Ingredient } from '../src/mygame/data/ingredient';
+
+test('adds 1 + 2 to equal 3', () => {
+    new Ingredient(0, "TestIngredient")
+});
